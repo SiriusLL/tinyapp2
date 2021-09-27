6f8a59b5d09eddfaff15c1bf5b9bc2c640bfbb72
SiriusLL <sirius.love.light@gmail.com>
2021-09-26 19:03:14 -0600

    url post request, random string generator function

--- express_server.js
+++ express_server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extend: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2, 8);
+};
+console.log(generateRandomString());
 app.get("/", (req, res) => {
   res.send("hello");
 });
@@ -26,6 +32,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   console.log("hello", req.params);
   const longURL = urlDatabase[req.params.shortURL];
@@ -33,6 +43,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
 
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("OK"); //Respond with 'OK' (we will replace this)
 });
 
 app.listen(PORT, () => {
